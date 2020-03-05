b7603f04553c9024e20b7e44ecba88b6a580dcc7
Meron Kebede <mkebede@Merons-MacBook-Pro-2.local>
2020-03-05 10:43:55 -0500

    routes

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var routes = require("./routes/routes.js");
+/**  Required External Modules*/
+const express = require("express");
+const bodyParser = require("body-parser");
+/** const routes = require("./routes/routes.js");*/
+const path = require("path");
 
-var app = express();
+/**  App Variables */
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**  App Configurations */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/images', express.static(__dirname + '/images'));
 app.use('/mocks', express.static(__dirname + '/mocks'));
-
-
 app.disable('etag');
 
-var server = app.listen(process.env.PORT || 8000, function () {
+/** Routes Definations*/
+app.get("/", (req, res) => {
+    res.status(200).send("Request Uber rides");
+  });
+
+const server = app.listen(process.env.PORT || 4000, function () {
     console.log("app running on port.", server.address().port);
 });
+
+
